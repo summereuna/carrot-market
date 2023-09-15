d9b3bfa1ec417a8d260ed6bcb1bc60392e9ea92b
summereuna <summereuna@gmail.com>
2023-09-15 17:53:48 +0900

    TIL: practice tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,105 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+import type { NextPage } from "next";
+const Home: NextPage = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
+        {/* 첫 번째: 계산서 */}
+        <div className="bg-white p-10 rounded-3xl shadow-xl">
+          <span className="font-semibold text-3xl">Select Item</span>
+          <div className="my-3 flex-col space-y-3">
+            <div className="flex justify-between">
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-gray-500">Tooly Table</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          </div>
+          <div className="flex justify-between pt-2 border-t-2 border-dashed">
+            <span>Total</span>
+            <span className="font-semibold">$19</span>
+          </div>
+          <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto block hover:bg-teal-500 active:bg-yellow-400 focus:text-blue-800">
+            Checkout
+          </button>
         </div>
+        {/* 두 번째: 프로필*/}
+        <div className="bg-white rounded-2xl shadow-xl overflow-hidden">
+          <div className="bg-blue-500 p-6 pb-14 flex justify-between text-2xl">
+            <span className="text-white font-semibold">Profile</span>
+          </div>
+          <div className="rounded-3xl p-6 relative -top-5 bg-white">
+            <div className="flex relative -top-16 items-end justify-between">
+              <div className="flex flex-col items-center">
+                <span className="text-sm text-gray-500">Order</span>
+                <span className="font-medium">613</span>
+              </div>
+              <div className="h-24 w-24 bg-yellow-400 rounded-full"></div>
+              <div className="flex flex-col items-center">
+                <span className="text-sm text-gray-500">Spent</span>
+                <span className="font-medium">$2013</span>
+              </div>
+            </div>
+            <div className="relative flex flex-col items-center -mt-5 ">
+              <span className="text-lg font-medium">Eunhwa Jung</span>
+              <span className="text-sm text-gray-500">Daegu, Korea</span>
+            </div>
+          </div>
+        </div>
+        {/* 세 번째: 제품 화면 */}
+        <div className="bg-white p-10 rounded-2xl shadow-xl">
+          <div className="flex justify-between items-center mb-3">
+            <span>⬅️</span>
+            <div className="space-x-3">
+              <span>⭐️ 4.7</span>
+              <span className="shadow-xl px-1 py-0.5 rounded-md text-red-500 border">
+                ♥️
+              </span>
+            </div>
+          </div>
+          <div className="bg-zinc-400 h-72 mb-5"></div>
+          <div className="flex flex-col">
+            <span className="font-medium text-lg">Swoon Lounge</span>
+            <span className="text-xs text-gray-500">Chair</span>
+            <div className="mt-3 mb-5 flex justify-between items-center">
+              <div className="space-x-2">
+                <button
+                  id="btn-yellow"
+                  className="w-4 h-4 rounded-full bg-yellow-400 focus:ring-2 ring-offset-2 ring-yellow-200 transition"
+                ></button>
+                <button
+                  id="btn-indigo"
+                  className="w-4 h-4 rounded-full bg-indigo-400 focus:ring-2 ring-offset-2 ring-indigo-200 transition"
+                ></button>
+                <button
+                  id="btn-teal"
+                  className="w-4 h-4 rounded-full bg-teal-400 focus:ring-2 ring-offset-2 ring-teal-200 transition"
+                ></button>
+              </div>
+              <div className="flex items-center space-x-5">
+                <button className="rounded-lg p-0.8 bg-blue-200 flex justify-center items-center w-8 aspect-square text-xl text-gray-500">
+                  -
+                </button>
+                <span>1</span>
+                <button className="rounded-lg p-0.8 bg-blue-200 flex justify-center items-center w-8 aspect-square text-xl text-gray-500">
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="font-medium text-2xl">$450</span>
+              <button className="bg-blue-500 text-center text-white rounded-lg px-5 py-2 text-sm">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* 네 번째 */}
+        <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  //어디서 타일윈드 사용할지 알리기
+  //페이지 폴더의 모든 폴더의 포든 파일의, 다음 확장자명에서...
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
