0a2a92588f6938a114dcd7b6cd2e28180be66ae2
summereuna <summereuna@gmail.com>
2023-09-18 16:00:40 +0900

    TIL: tailwind responsive modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,111 @@
 import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-10 p-5">
-      <div>
-        <h2 className="font-semibold text-lg">✅ details 태그</h2>
-        <details className="select-none open:text-white open:bg-red-400">
-          <summary className="cursor-pointer">
-            디테일 태그의 제목 쓰는 부분: 그냥 html임
-          </summary>
-          <span>컨텐츠츠츠</span>
-        </details>
+    <div className="bg-slate-400 py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen xl:place-content-center">
+      {/* 첫 번째: 계산서 */}
+      <div className="bg-white p-10 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="my-3 flex-col space-y-3">
+          <div className="flex justify-between">
+            <span className="text-gray-500">Grey Chair</span>
+            <span className="font-semibold">$19</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-500">Tooly Table</span>
+            <span className="font-semibold">$19</span>
+          </div>
+        </div>
+        <div className="flex justify-between pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto block hover:bg-teal-500 active:bg-yellow-400 focus:text-blue-800 transition">
+          Checkout
+        </button>
       </div>
-
-      <div>
-        <h2 className="font-semibold text-lg">
-          ✅ 유저가 글자 선택했을 때 색 변경 selection:bg-indigo-500
-        </h2>
-        <span className="selection:bg-indigo-500 selection:text-white">
-          유저가 글자 선택했을 때 색 변경
-        </span>
+      {/* 두 번째: 프로필*/}
+      <div className="bg-white rounded-2xl shadow-xl overflow-hidden group">
+        <div className="bg-blue-500 p-6 pb-14 xl:pb-40 flex justify-between text-2xl">
+          <span className="text-white font-semibold">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 relative -top-5 bg-white">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Order</span>
+              <span className="font-medium">613</span>
+            </div>
+            <div className="h-24 w-24 bg-gray-300 rounded-full group-hover:bg-yellow-400 transition" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2013</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 ">
+            <span className="text-lg font-medium">Eunhwa Jung</span>
+            <span className="text-sm text-gray-500">Daegu, Korea</span>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <h2 className="font-semibold text-lg">✅ ul - li</h2>
-        <ul className="list-decimal marker:text-teal-500">
-          <li>hi~</li>
-          <li>hi~</li>
-          <li>hi~</li>
-        </ul>
+      {/* 세 번째: 제품 화면 */}
+      <div className="bg-white p-10 rounded-2xl shadow-xl lg:col-span-2 xl:col-span-1">
+        <div className="flex justify-between items-center mb-3">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.7</span>
+            <span className="shadow-xl px-1 py-0.5 rounded-md text-red-500 border">
+              ♥️
+            </span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5"></div>
+        <div className="flex flex-col">
+          <span className="font-medium text-lg">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button
+                id="btn-yellow"
+                className="w-4 h-4 rounded-full bg-yellow-400 focus:ring-2 ring-offset-2 ring-yellow-200 transition"
+              ></button>
+              <button
+                id="btn-indigo"
+                className="w-4 h-4 rounded-full bg-indigo-400 focus:ring-2 ring-offset-2 ring-indigo-200 transition"
+              ></button>
+              <button
+                id="btn-teal"
+                className="w-4 h-4 rounded-full bg-teal-400 focus:ring-2 ring-offset-2 ring-teal-200 transition"
+              ></button>
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="rounded-lg p-0.8 bg-blue-200 flex justify-center items-center w-8 aspect-square text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="rounded-lg p-0.8 bg-blue-200 flex justify-center items-center w-8 aspect-square text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 text-center text-white rounded-lg px-5 py-2 text-sm">
+              Add to cart
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <h2 className="font-semibold text-lg">
-          ✅ file selector and modifier file:border-0{" "}
-        </h2>
-        <input
-          type="file"
-          className="file:border-0 file:rounded-xl file:bg-purple-600 file:px-3 file:py-2 file:text-white bg-purple-200 rounded-xl"
-        />
-      </div>
-
-      <div>
-        <h2 className="font-semibold text-lg">
-          ✅ 여러 modifier 중첩(stack)시키기 file:hover:bg-white
-        </h2>
-        <input
-          type="file"
-          className="file:cursor-pointer file:hover:text-purple-600 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:transition-colors file:border-0 file:rounded-xl file:bg-purple-600 file:px-3 file:py-2 file:text-white bg-purple-200 rounded-xl"
-        />
-      </div>
-
-      <div>
-        <h2 className="font-semibold text-lg">
-          ✅ 문장 첫 글자 크게 first-letter:text-6xl / first-line:bg-yellow-300
-        </h2>
-        <p className="first-letter:text-6xl font-serif first-letter:hover:text-indigo-300 hover:first-line:bg-yellow-300">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
-          animi quae placeat soluta iusto autem. Autem distinctio dolorum ea
-          voluptate velit dolore cupiditate officiis fugit laudantium?
-          Exercitationem id voluptates mollitia.
-        </p>
-      </div>
+      {/*
+        반응형을 만들 때, 사람들은 보통 먼저 데스크탑 버전 부터 생각한 후 그걸 모바일 버전에 적용시킴
+        tailwind는 뒤집어져 있음, 모든 클래스가 모바일에 우선 적용된다.
+        그러고 나서 큰 화면을 위해 디자인을 변경해야 한다.
+        크게 만들면 못생...
+        화면이 커지면 좌우로 3열이 정렬되게 보이게 만들어보자
+        - [] sm
+        - [] md
+        - [] lg
+        - [] xl
+        - [] 2xl
+  */}
     </div>
   );
 };
