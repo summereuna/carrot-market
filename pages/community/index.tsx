d4bd5d2581fa4aa74554d50d02a54c9bc4d95b31
summereuna <summereuna@gmail.com>
2023-09-23 00:31:52 +0900

    Add: community write page

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -60,7 +60,7 @@ const Community: NextPage = () => {
       ))}
 
       {/*플로팅 버튼: 클릭 시 상품 추가하는 페이지로*/}
-      <Link href="/items/upload">
+      <Link href="/community/write">
         <button className="flex space-x-1 fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl transition-colors hover:bg-orange-500 cursor-pointer">
           <svg
             className="h-6 w-6"
---
+++ pages/community/write.tsx
@@ -0,0 +1,54 @@
+import type { NextPage } from "next";
+
+const Write: NextPage = () => {
+  return (
+    <div className="px-4 py-10 space-y-2">
+      <div
+        className="cursor-pointer flex items-center justify-between px-3 py-3 text-gray-800 border-b border-gray-300
+        "
+      >
+        <span>동네질문</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </div>
+      <div className="p-4 bg-gray-200 rounded-xl">
+        <span className="text-xs font-bold mr-2">안내</span>
+        <span className="text-xs">
+          중고거래 관련, 명예훼손, 광고/홍보 목적의 글은 올리실 수 없어요.
+        </span>
+      </div>
+      <form>
+        <input
+          type="text"
+          placeholder="제목을 입력하세요"
+          required
+          className="text-gray-800 font-bold text-lg placeholder:text-gray-500 placeholder:font-bold placeholder:text-lg  appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-400 focus:border-orange-400
+                "
+        />
+        <textarea
+          id="community_question_write"
+          rows={5}
+          placeholder="가까이 사는 동네 이웃들에게 궁금한 것을 물어보세요! 근처 이웃이 친절하게 진짜 동네 정보를 알려줄거예요"
+          className="text-gray-800 mt-1 shadow-sm w-full border-gray-300 focus:outline-none focus:ring-orange-400 focus:border-orange-400 rounded-md "
+        />
+        <button className="w-full mt-2 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+          완료
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
