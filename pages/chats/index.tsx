0623a8dc3377dbf3bee2189da288702734febc02
summereuna <summereuna@gmail.com>
2023-09-29 00:11:47 +0900

    Refactor: chat page

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,21 +1,27 @@
 import Layout from "@/components/layout";
+import User from "@/components/user";
 import type { NextPage } from "next";
 import Link from "next/link";
 
 const Chats: NextPage = () => {
   return (
     <Layout title="채팅" hasTabBar>
-      <div className="py-3 divide-y-[1px] ">
-        {[1, 1, 1].map((_, i) => (
+      <div className="divide-y-[1px] ">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <div key={i}>
             <Link href="/chats/userId">
-              <div className="flex px-4 cursor-pointer py-3 items-center space-x-3">
-                <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div className="cursor-pointer px-4 py-3 flex space-x-3 items-center">
                 <div>
-                  <p className="text-gray-700">참치맛오이</p>
-                  <p className="text-sm  text-gray-500">
+                  <div className="w-12 h-12 rounded-full bg-slate-300" />
+                </div>
+                <div className="flex flex-col">
+                  <div className="space-x-2">
+                    <span className="font-medium text-gray-700">당근케익</span>
+                    <span className="text-xs text-gray-500">오전 12:50</span>
+                  </div>
+                  <span className="text-sm text-gray-500">
                     오후 7시 이후 역 앞에서 가능하신가요?
-                  </p>
+                  </span>
                 </div>
               </div>
             </Link>
