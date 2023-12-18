609b8816c5ac5b348a5845dd33d8be4fa7dcb7bb
summereuna <summereuna@gmail.com>
2023-12-18 16:29:42 +0900

    style: modify community [id] page

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -108,104 +108,92 @@ const CommunityPostDetail: NextPage = () => {
   }, [answerData, resetAnswerForm, boundMutate]);
   return (
     <Layout canGoBack>
-      <div>
+      <div className="border-b-8 border-slate-100 w-full space-y-5 py-3">
         <span
-          className="inline-flex my-3 ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800
+          className="ml-4 inline-flex my-3 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800
         "
         >
           동네질문
         </span>
         {/*user-profile"*/}
         <Link href={`/users/profiles/${data?.post?.userId}`}>
-          <div className="px-4 mb-3 py-3 border-b">
+          <div className="px-4">
             <UserBox
               name={data?.post?.user.name}
               size="small"
-              time={data?.post?.created.toString()}
+              time={data?.post?.created}
               avatar={data?.post?.user.avatar}
             />
           </div>
         </Link>
-        <div>
-          <div className="mt-2 px-4 text-gray-700 flex flex-col space-y-2">
-            <div className="text-xl font-semibold">
-              <span className="text-orange-500">Q. </span>
-              <span>{data?.post?.title}</span>
-            </div>
-            <p>{data?.post?.content}</p>
+        {/* 내용 */}
+        <div className="px-4 text-gray-700 flex flex-col space-y-3">
+          <div className="text-xl font-semibold">
+            <span>{data?.post?.title}</span>
           </div>
-          <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
+          <p>{data?.post?.content}</p>
+          <div className="py-1">
             <button
               onClick={onRecommendationClick}
               className={cls(
-                "flex space-x-2 items-center text-sm focus:outline-none",
-                data?.isRecommend ? "text-green-400" : ""
+                "flex space-x-2 items-center text-sm focus:outline-none border-[1px] py-1 px-3 rounded-2xl",
+                data?.isRecommend ? "text-orange-400 border-orange-400" : ""
               )}
             >
               <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
+                  d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+                />
               </svg>
-              <span>추천 {data?.post?._count.recommendations}</span>
+              <span>{data?.post?._count.recommendations}</span>
             </button>
-            <span className="flex space-x-2 items-center text-sm">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>답변 {data?.post?._count.answers}</span>
-            </span>
           </div>
         </div>
-        {/*댓글*/}
-        <div className="px-4 my-5 space-y-5">
-          {data?.post?.answers?.map((answer) => (
-            <Comment
-              key={answer.id}
-              name={answer.user.name}
-              time={answer.updated.toString()}
-              comment={answer.answer}
-              avatar={answer.user.avatar}
-            />
-          ))}
-        </div>
-        <form className="px-4 space-y-2" onSubmit={handleSubmit(onValid)}>
-          <Textarea
-            register={register("answer", {
-              required: true,
-              minLength: {
-                value: 2,
-                message: "댓글을 2글자 이상 입력해주세요.",
-              },
-            })}
-            name="answer"
-            placeholder="댓글을 달아주세요."
-            required
-          />
-          {errors?.answer ? <p>{errors.answer?.message}</p> : null}
-          <Button loading={answerLoading} text="댓글달기" />
-        </form>
       </div>
+      {/* 내용 */}
+
+      {/*댓글*/}
+      <div className="px-4 my-5 space-y-5">
+        <div className="flex space-x-2 items-center text-gray-700 text-sm">
+          <span>댓글 {data?.post?._count.answers}</span>
+        </div>
+        {data?.post?.answers?.map((answer) => (
+          <Comment
+            key={answer.id}
+            name={answer.user.name}
+            time={answer.updated.toString()}
+            comment={answer.answer}
+            avatar={answer.user.avatar}
+          />
+        ))}
+      </div>
+
+      {/*댓글 입력 폼*/}
+      <form className="px-4 space-y-2" onSubmit={handleSubmit(onValid)}>
+        <Textarea
+          register={register("answer", {
+            required: true,
+            minLength: {
+              value: 2,
+              message: "댓글을 2글자 이상 입력해주세요.",
+            },
+          })}
+          name="answer"
+          placeholder="댓글을 달아주세요."
+          required
+        />
+        {errors?.answer ? <p>{errors.answer?.message}</p> : null}
+        <Button loading={answerLoading} text="댓글달기" />
+      </form>
     </Layout>
   );
 };
