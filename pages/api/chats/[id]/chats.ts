8f6c0de67bfcc49bfbb3b888b6b4d01d0d8500df
summereuna <summereuna@gmail.com>
2024-01-06 05:56:35 +0900

    Fix: update the chatroom when sending a chat

--- pages/api/chats/[id]/chats.ts
+++ pages/api/chats/[id]/chats.ts
@@ -1,3 +1,4 @@
+import { ChatRoom } from "@prisma/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@/libs/server/client";
@@ -19,6 +20,14 @@ async function handler(
       isReservedAlarm: false,
       user: { connect: { id: user?.id } },
       chatRoom: { connect: { id: +id!.toString() } },
+    },
+  });
+
+  //채팅룸 업데이트> 채팅룸에 챗 생기면 업뎃 시키고 싶은데 걍 updatedAt만 바뀌게..어떻게 하지
+  await client.chatRoom.update({
+    where: { id: +id!.toString() },
+    data: {
+      updated: new Date(),
     },
   });
 
