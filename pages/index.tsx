12d7ccbc48952b9334b1fedaefa6ba821b0399a5
summereuna <summereuna@gmail.com>
2024-03-02 00:28:34 +0900

    Feat: use useSWR with getServerSideProps to pre render products data on the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import FloatingButton from "@/components/FloatingButton";
 import Item from "@/components/Item";
 import Layout from "@/components/Layout";
 import Seo from "@/components/Seo";
-import useUser from "@/libs/client/useUser";
 import { Product } from "@prisma/client";
 import type { NextPage } from "next";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
+import client from "@/libs/server/client";
 
 export interface ProductWithCountWishesAndStateChecks extends Product {
   _count: { wishes: number };
@@ -18,13 +18,11 @@ interface ProductsResponse {
   products: ProductWithCountWishesAndStateChecks[];
 }
 
-//상품 리스트
 const Home: NextPage = () => {
-  const { user, isLoading } = useUser();
-  //console.log(user);
-
+  //4. Page 컴포넌트 안엫서 캐시 초기값을 설정해 뒀기 때문에
+  //CSR에 있는 useSWR은 캐시에서 데이터를 불러오게 된다.
+  //따라서 로딩 상태는 안 보이고 바로 데이터를 가져오는 것 처럼 보인다
   const { data } = useSWR<ProductsResponse>("/api/products");
-  console.log(data?.products);
 
   return (
     <Layout title="홈" hasTabBar>
@@ -54,4 +52,52 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+//3. Page 컴포넌트가 Home 컴포넌트를 SWRConfig로 감싸줌
+//SWRConfig 컴포넌트는 fallback 프로퍼티로 캐시 초기 값을 설정할 수 있음
+//그러면 Home 컴포넌트가 api주소 키를 이용하여 캐시를 불러온다.
+//캐시 값으로 api의 리턴 값을 가져오면 된다.
+const Page: NextPage<{ products: ProductWithCountWishesAndStateChecks[] }> = ({
+  products,
+}) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/products": {
+            ok: true,
+            products,
+          },
+        },
+      }}
+    >
+      <Home />
+    </SWRConfig>
+  );
+};
+
+//1. getServerSideProps 함수 안에서 products를 가져온다.
+export async function getServerSideProps() {
+  const products = await client.product.findMany({
+    //거기에 wishes 카운트도 포함해서
+    include: {
+      _count: {
+        select: { wishes: true },
+      },
+      reservation: { select: { id: true } },
+      review: { select: { id: true } },
+    },
+    orderBy: { created: "desc" },
+  });
+
+  // res.json({
+  //   ok: true,
+  //   products,
+  // });
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) },
+    // Error serializing props ~ 에러 해결 위해 parse
+  };
+}
+
+//2. 내보내는 컴포넌트는 상품 정보를 prop으로 받는 컴포넌트인 Page 컴포넌트로
+export default Page;
